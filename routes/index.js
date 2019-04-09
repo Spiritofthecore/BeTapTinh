cd68a38f33ce4ee3f7cad5c46a423853cf8bac10
Nguyễn Hoàng Cương Lê <hoangcuong9331@gmail.com>
2019-04-09 08:27:59 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+router.get('/', function(req, res) {
+  res.sendFile('views/default.html', {'root': './'})
 });
+
 
 module.exports = router;
